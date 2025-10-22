b73477607d82b57db2836e1a57e12b61b2eae8fe
anassabort404 <anass.elharrati-etu@etu.univh2c.ma>
2025-10-22 19:59:43 +0100

    Adding the discovery, gateway and config services

--- keynote-service/src/main/java/net/anassploit/keynoteservice/repository/KeynoteRepository.java
+++ keynote-service/src/main/java/net/anassploit/keynoteservice/repository/KeynoteRepository.java
@@ -3,6 +3,6 @@ package net.anassploit.keynoteservice.repository;
 import net.anassploit.keynoteservice.entities.Keynote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface KeynoteRepository extends JpaRepository<Keynote, String> {
+public interface KeynoteRepository extends JpaRepository<Keynote, Long> {
 
 }
